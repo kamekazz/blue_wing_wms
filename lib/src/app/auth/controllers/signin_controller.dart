e9f13854c7658e80018d620c801ace0eb998304f
manuel taveras <32987336+kamekazz@users.noreply.github.com>
2022-12-17 00:18:03 -0500

    login firebase

--- lib/src/app/auth/controllers/signin_controller.dart
+++ lib/src/app/auth/controllers/signin_controller.dart
@@ -2,18 +2,16 @@ import 'package:blue_wing_wms/src/repository/authentication_repository/authentic
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class SignUpController extends GetxController {
-  static SignUpController get instance => Get.find();
+class LoginController extends GetxController {
+  static LoginController get instance => Get.find();
 
   //textfield controller to get data from TextFields
 
   final email = TextEditingController();
   final password = TextEditingController();
-  final fullName = TextEditingController();
-  final phoneNo = TextEditingController();
 
-  void registerUser(String email, String password) {
+  void loginUser(String email, String password) {
     AuthenticationRepository.instance
-        .createUserWithEmailAndPassword(email, password);
+        .loginUserWithEmailAndPassword(email, password);
   }
 }
--- lib/src/app/auth/widgets/login/login_form.dart
+++ lib/src/app/auth/widgets/login/login_form.dart
@@ -1,9 +1,11 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:blue_wing_wms/src/app/auth/controllers/signin_controller.dart';
 import 'package:blue_wing_wms/src/app/auth/widgets/forget_password/forget_password_options/forget_password_model_bottom_sheet.dart';
 import 'package:flutter/material.dart';
 
 import 'package:blue_wing_wms/src/constants/sizes.dart';
 import 'package:blue_wing_wms/src/constants/text_strings.dart';
+import 'package:get/get.dart';
 
 class LoginForm extends StatelessWidget {
   const LoginForm({
@@ -12,51 +14,62 @@ class LoginForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(LoginController());
+    final _formKey = GlobalKey<FormState>();
     return Form(
+        key: _formKey,
         child: Container(
-      padding: const EdgeInsets.symmetric(vertical: 20.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          TextFormField(
-              decoration: const InputDecoration(
-                  prefixIcon: Icon(Icons.person_outline_outlined),
-                  labelText: ttEmail,
-                  hintText: ttEmail,
-                  border: OutlineInputBorder())),
-          const SizedBox(
-            height: dtFormHeight,
+          padding: const EdgeInsets.symmetric(vertical: 20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextFormField(
+                  controller: controller.email,
+                  decoration: const InputDecoration(
+                      prefixIcon: Icon(Icons.person_outline_outlined),
+                      labelText: ttEmail,
+                      hintText: ttEmail,
+                      border: OutlineInputBorder())),
+              const SizedBox(
+                height: dtFormHeight,
+              ),
+              TextFormField(
+                  controller: controller.password,
+                  decoration: const InputDecoration(
+                      prefixIcon: Icon(Icons.fingerprint),
+                      labelText: ttPassword,
+                      hintText: ttPassword,
+                      border: OutlineInputBorder(),
+                      suffixIcon: IconButton(
+                          onPressed: null,
+                          icon: Icon(Icons.remove_red_eye_sharp)))),
+              const SizedBox(
+                height: dtFormHeight - 20,
+              ),
+              Align(
+                alignment: Alignment.centerRight,
+                child: TextButton(
+                  onPressed: () {
+                    ForgetPasswordScreen.buildShowModalBottomSheet(context);
+                  },
+                  child: const Text(ttForgetPassword),
+                ),
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      LoginController.instance.loginUser(
+                          controller.email.text.trim(),
+                          controller.password.text.trim());
+                    }
+                  },
+                  child: Text(ttLogin.toLowerCase()),
+                ),
+              )
+            ],
           ),
-          TextFormField(
-              decoration: const InputDecoration(
-                  prefixIcon: Icon(Icons.fingerprint),
-                  labelText: ttPassword,
-                  hintText: ttPassword,
-                  border: OutlineInputBorder(),
-                  suffixIcon: IconButton(
-                      onPressed: null,
-                      icon: Icon(Icons.remove_red_eye_sharp)))),
-          const SizedBox(
-            height: dtFormHeight - 20,
-          ),
-          Align(
-            alignment: Alignment.centerRight,
-            child: TextButton(
-              onPressed: () {
-                ForgetPasswordScreen.buildShowModalBottomSheet(context);
-              },
-              child: const Text(ttForgetPassword),
-            ),
-          ),
-          SizedBox(
-            width: double.infinity,
-            child: ElevatedButton(
-              onPressed: () {},
-              child: Text(ttLogin.toLowerCase()),
-            ),
-          )
-        ],
-      ),
-    ));
+        ));
   }
 }
